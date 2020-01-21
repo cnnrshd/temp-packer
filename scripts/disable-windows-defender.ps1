0b308b8260a8ac4d48398fe15aed098e4f0bee0f
connor <cws4197@rit.edu>
2020-01-20 19:22:01 -0500

    change local path to reg path

--- scripts/disable-windows-defender.ps1
+++ scripts/disable-windows-defender.ps1
@@ -165,4 +165,4 @@ Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\WdNisSvc" "AutorunsDis
 Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Sense" "Start" 4
 Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Sense" "AutorunsDisabled" 3
 Takeown-Registry("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Features")
-Set-ItemProperty "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Features" "TamperProtection" "0"
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows Defender\Features" "TamperProtection" "0"
