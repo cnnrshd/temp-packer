ca71d41b1af9f6064f5671f4dcfd6e2999381a08
connor <cws4197@rit.edu>
2020-01-20 21:15:03 -0500

    fix sysprep - add template unattend file to http share - add templatting script (get-unattend.ps1) - change sysprep.bat, it now only generalizes quietly with the unattend - changes have only been made to 2019corehyperv

--- scripts/sysprep.bat
+++ scripts/sysprep.bat
@@ -1,7 +1,2 @@
-net stop tiledatamodelsvc
-if exist a:\Autounattend.xml (
-  c:\windows\system32\sysprep\sysprep.exe /generalize /unattend:a:\Autounattend.xml
-) else (
-  del /F \Windows\System32\Sysprep\unattend.xml
-  c:\windows\system32\sysprep\sysprep.exe /generalize
-)
+C:\windows\system32\sysprep\sysprep.exe /generalize /unattend:C:\unattend.xml /quiet
+del /F C:\unattend.xml
